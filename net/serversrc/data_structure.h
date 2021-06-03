a9ce78dbc1964cd6bdb5c62c9dab5e6f7a661eb4
Kevin Kellar <kellar.kevin@gmail.com>
2021-06-03 10:00:33 -0700

    Stolen relevant info from colorid branch

--- net/serversrc/data_structure.h
+++ net/serversrc/data_structure.h
@@ -7,15 +7,23 @@
 #include <string>
 #include <vector>
 
+typedef struct
+{
+    int unique_color_id;
+} PersistentInfo;
+
 void clientFDListAddFd(int fd);
 __attribute__((warn_unused_result)) int clientFDListIndexof(int fd);
 __attribute__((warn_unused_result)) bool clientFDListCheckExistence(int fd);
 __attribute__((warn_unused_result)) bool clientFDListRemoveFd(int fd);
 __attribute__((warn_unused_result)) const std::list<int> clientFdListGetAll();
-__attribute__((warn_unused_result)) bool clientMapCheckHandleExistence(const std::string handle);
-void clientMapAddHandle(const std::string handle, int fd);
-__attribute__((warn_unused_result)) bool clientMapDeleteHandle(const std::string handle);
-__attribute__((warn_unused_result)) int clientMapFindByHandle(const std::string handle);
+__attribute__((warn_unused_result)) bool clientMapCheckHandleExistence(
+    const std::string handle);
+__attribute__((warn_unused_result)) PersistentInfo clientMapAddHandle(const std::string handle, int fd);
+__attribute__((warn_unused_result)) bool clientMapDeleteHandle(
+    const std::string handle);
+__attribute__((warn_unused_result)) int clientMapFindByHandle(
+    const std::string handle);
 __attribute__((warn_unused_result)) bool clientMapDoesFdExist(int fd);
 __attribute__((warn_unused_result)) const std::string clientMapFindByFD(int fd);
 __attribute__((warn_unused_result)) const std::vector<std::string> clientMapGetAllHandles();
