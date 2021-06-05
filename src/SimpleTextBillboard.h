05d8ca5fdb70b81ed09b640a936772cd74970bcf
Kevin Kellar <kellar.kevin@gmail.com>
2021-06-05 12:48:05 -0700

    customtext things

---
+++ src/SimpleTextBillboard.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <memory>
+#include <string>
+
+#include "Program.h"
+#include "AnimTextureBillboard.h"
+
+class CustomTextBillboard
+{
+   private:
+    GLuint VertexArrayID;
+    GLuint VertexBufferID;
+    GLuint VertexNormDBox;
+    GLuint VertexTexBox;
+    GLuint IndexBufferIDBox;
+    GLuint Texture;
+    std::shared_ptr<Program> prog;
+
+   public:
+    void initGeom(const std::string& resDir);
+    void initProgram(const std::string& resourceDirectory
+                     );
+    void initTexture(const std::string& resourceDirectory,ImageLoader loader);
+    void renderLaser(glm::mat4& P,
+                     glm::mat4& V,
+                     glm::vec3 campos,
+                     glm::vec3 position_xyz,
+                     glm::vec3 modify_color,
+                     float time);
+};
