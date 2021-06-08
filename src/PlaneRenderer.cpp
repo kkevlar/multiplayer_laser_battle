9c979d574986e507b2e5e8eee2e81669925f25ac
Kevin Kellar <kellar.kevin@gmail.com>
2021-06-08 10:23:28 -0700

    formatonly

--- src/PlaneRenderer.cpp
+++ src/PlaneRenderer.cpp
@@ -37,85 +37,85 @@ static mat4 safe_lookat(vec3 me, vec3 target, vec3 up)
     return m1;
 }
 
-    void PlaneRenderer::initGeom(const std::string& resDir, PlaneImageLoader loader)
+void PlaneRenderer::initGeom(const std::string& resDir, PlaneImageLoader loader)
 {
+    plane = make_shared<Shape>();
+    string mtldir = resDir + "/FA-18E_SuperHornet/";
+    plane->loadMesh(resDir + "/FA-18E_SuperHornet/FA-18E_SuperHornet.obj", &mtldir, loader);
 
-        plane = make_shared<Shape>();
-        string mtldir = resDir + "/FA-18E_SuperHornet/";
-        plane->loadMesh(resDir + "/FA-18E_SuperHornet/FA-18E_SuperHornet.obj", &mtldir, loader);
-
-        plane->resize();
-        plane->init();
+    plane->resize();
+    plane->init();
 }
 
 void PlaneRenderer::initProgram(const std::string& resourceDirectory)
 {
-        internal_plane_prog = std::make_shared<Program>();
-        internal_plane_prog->setVerbose(true);
-        internal_plane_prog->setShaderNames(resourceDirectory + "/plane_vertex.glsl", resourceDirectory + "/plane_frag.glsl");
-        if (!internal_plane_prog->init())
-        {
-            std::cerr << "One or more shaders failed to compile... exiting!" << std::endl;
-            exit(1);
-        }
-        internal_plane_prog->addUniform("P");
-        internal_plane_prog->addUniform("V");
-        internal_plane_prog->addUniform("M");
-        internal_plane_prog->addUniform("campos");
-        internal_plane_prog->addUniform("tint_color");
-        internal_plane_prog->addAttribute("vertPos");
-        internal_plane_prog->addAttribute("vertNor");
-        internal_plane_prog->addAttribute("vertTex");
+    internal_plane_prog = std::make_shared<Program>();
+    internal_plane_prog->setVerbose(true);
+    internal_plane_prog->setShaderNames(resourceDirectory + "/plane_vertex.glsl",
+                                        resourceDirectory + "/plane_frag.glsl");
+    if (!internal_plane_prog->init())
+    {
+        std::cerr << "One or more shaders failed to compile... exiting!" << std::endl;
+        exit(1);
+    }
+    internal_plane_prog->addUniform("P");
+    internal_plane_prog->addUniform("V");
+    internal_plane_prog->addUniform("M");
+    internal_plane_prog->addUniform("campos");
+    internal_plane_prog->addUniform("tint_color");
+    internal_plane_prog->addAttribute("vertPos");
+    internal_plane_prog->addAttribute("vertNor");
+    internal_plane_prog->addAttribute("vertTex");
 }
 
 void PlaneRenderer::renderLaser(glm::mat4& P,
-                                       glm::mat4& V,
-                                       glm::vec3 campos,
-                                       glm::vec3 position_xyz,
-                                       glm::vec3 where_to_look,
-                                       glm::vec3 modify_color,
-                                       float time)
+                                glm::mat4& V,
+                                glm::vec3 campos,
+                                glm::vec3 position_xyz,
+                                glm::vec3 where_to_look,
+                                glm::vec3 modify_color,
+                                float time)
 {
-//TODO TODO
-        // pplane->bind();
-        // glUniformMatrix4fv(pplane->getUniform("P"), 1, GL_FALSE, &P[0][0]);
-        // glUniformMatrix4fv(pplane->getUniform("V"), 1, GL_FALSE, &V[0][0]);
-        // glUniformMatrix4fv(pplane->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-        // glUniform3fv(pplane->getUniform("campos"), 1, &mycam.pos[0]);
-        // glUniform3fv(pplane->getUniform("tint_color"), 1, &my_allocated_color_from_server[0]);
-        // glActiveTexture(GL_TEXTURE0);
-        // glBindTexture(GL_TEXTURE_2D, Texture2);
-        // plane->draw(pplane);  // render!!!!!!
+    // TODO TODO
+    // pplane->bind();
+    // glUniformMatrix4fv(pplane->getUniform("P"), 1, GL_FALSE, &P[0][0]);
+    // glUniformMatrix4fv(pplane->getUniform("V"), 1, GL_FALSE, &V[0][0]);
+    // glUniformMatrix4fv(pplane->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+    // glUniform3fv(pplane->getUniform("campos"), 1, &mycam.pos[0]);
+    // glUniform3fv(pplane->getUniform("tint_color"), 1, &my_allocated_color_from_server[0]);
+    // glActiveTexture(GL_TEXTURE0);
+    // glBindTexture(GL_TEXTURE_2D, Texture2);
+    // plane->draw(pplane);  // render!!!!!!
 
-        // const auto estimates = network.GiveOtherPlaneEstimates(glfwGetTime());
-        // for (const auto& estimate : estimates)
-        // {
-        //     translate_plane = translate(mat4(1), estimate.pos);
-        //     plane_overall_rot = mat4(quat(estimate.rot));
+    // const auto estimates = network.GiveOtherPlaneEstimates(glfwGetTime());
+    // for (const auto& estimate : estimates)
+    // {
+    //     translate_plane = translate(mat4(1), estimate.pos);
+    //     plane_overall_rot = mat4(quat(estimate.rot));
 
-        //     M = translate_plane * plane_overall_rot * scale_plane;
-        //     glUniformMatrix4fv(pplane->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-        //     glUniform3fv(pplane->getUniform("tint_color"), 1, &estimate.color[0]);
-        //     plane->draw(pplane);  // render!!!!!!
-        // }
+    //     M = translate_plane * plane_overall_rot * scale_plane;
+    //     glUniformMatrix4fv(pplane->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+    //     glUniform3fv(pplane->getUniform("tint_color"), 1, &estimate.color[0]);
+    //     plane->draw(pplane);  // render!!!!!!
+    // }
 
-        // draw the bots
+    // draw the bots
 
-//TODO TODO
-        // for (int i = 0; i < thebots.size(); i++)
-        // {
-        //     // scale_plane = scale(mat4(1), vec3(10));
-        //     rotate_plane = safe_lookat(thebots[i].pos, thebots[i].pos + thebots[i].forward, thebots[i].up);
-        //     translate_plane = translate(mat4(1), thebots[i].pos);
-        //     plane_overall_rot = rotate_plane * rotate_default_plane;
-        //     M = translate_plane * plane_overall_rot * scale_plane;
+    // TODO TODO
+    // for (int i = 0; i < thebots.size(); i++)
+    // {
+    //     // scale_plane = scale(mat4(1), vec3(10));
+    //     rotate_plane = safe_lookat(thebots[i].pos, thebots[i].pos + thebots[i].forward, thebots[i].up);
+    //     translate_plane = translate(mat4(1), thebots[i].pos);
+    //     plane_overall_rot = rotate_plane * rotate_default_plane;
+    //     M = translate_plane * plane_overall_rot * scale_plane;
 
-        //     glUniformMatrix4fv(pplane->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-        //     glUniform3fv(pplane->getUniform("tint_color"), 1, &my_allocated_color_from_server[0]);
-        //     glActiveTexture(GL_TEXTURE0);
-        //     glBindTexture(GL_TEXTURE_2D, Texture2);
-        //     plane->draw(pplane);  // render!!!!!!
-        // }
+    //     glUniformMatrix4fv(pplane->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+    //     glUniform3fv(pplane->getUniform("tint_color"), 1, &my_allocated_color_from_server[0]);
+    //     glActiveTexture(GL_TEXTURE0);
+    //     glBindTexture(GL_TEXTURE_2D, Texture2);
+    //     plane->draw(pplane);  // render!!!!!!
+    // }
 
-        // pplane->unbind();
+    // pplane->unbind();
 }
